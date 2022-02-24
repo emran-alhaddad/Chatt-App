8a3a1d83d97cdf60ea885349046655669171a45c
emranco <emranhadad770774255770774255@gmail.com>
2022-02-24 21:18:18 +0300

    Start init

--- server.js
+++ server.js
@@ -72,5 +72,5 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 2222;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
